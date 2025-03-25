8d18328534bd983376a838c01765cdb17cd768fd
Raúl Gutiérrez Merino <loscanos3raul@gmail.com>
2025-03-25 21:16:50 +0100

    protegiendo rutas

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\MidAdmin;
+use App\Http\Middleware\MidAlumno;
+use App\Http\Middleware\MidProfesor;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo('api/nologin');
+        $middleware->alias([
+            'midProfesor' => MidProfesor::class,
+            'midAdmin' => MidAdmin::class,
+            'midAlumno' => MidAlumno::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- server/routes/api.php
+++ server/routes/api.php
@@ -7,20 +7,21 @@ use App\Http\Controllers\UserRolController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('user')->group(function() {
+Route::prefix('user')->middleware('auth:sanctum')->group(function() {
     Route::get('/', [UserController::class, 'getUser']);
     Route::get('/{id}', [UserController::class, 'getUserById']);
     Route::put('/{id}', [UserController::class, 'putUser']);
     Route::get('/userWithRol/more/info', [UserController::class, 'getUsersWithUserRol']);
-    Route::get('/ifMailExist/{email}', [UserController::class, 'getIfEmailExist']);
 });
+
+Route::get('user/ifMailExist/{email}', [UserController::class, 'getIfEmailExist']);
 
 Route::prefix('rol')->group(function() {
     Route::get('/', [RolController::class, 'getRol']);
     Route::get('/{id}', [RolController::class, 'getRolById']);
 });
 
-Route::prefix('userRol')->group(function() {
+Route::prefix('userRol')->middleware(['auth:sanctum', 'midAdmin'])->group(function() {
     Route::get('/', [UserRolController::class, 'getUserRol']);
     Route::get('/usuario/{id}', [UserRolController::class, 'getUserRolByusuarioId']);
     Route::get('/rol/{id}', [UserRolController::class, 'getUserRolByRolId']);
@@ -32,3 +33,7 @@ Route::prefix('userRol')->group(function() {
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout']);
+
+Route::get('/nologin', function () {
+    return response()->json("No autorizado",203);
+});
